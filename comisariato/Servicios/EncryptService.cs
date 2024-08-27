2436c2be03b318a9cc1f14f1ad55adab08cac65a
Erick Quispal <equispalt@gmail.com>
2024-08-26 23:48:48 -0600

    definicion de front end Login

---
+++ comisariato/Servicios/EncryptService.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace comisariato.Servicios
+{
+    public class EncryptService
+    {
+        public static string ConvertirSHA256(string texto)
+        {
+            // referencia de "System.security.cryptography"
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+            return Sb.ToString();
+        }
+
+    }
+}
---
+++ comisariato/Servicios/PermisosService.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using comisariato.Models;
+using Microsoft.Data.SqlClient;
+
+
+namespace comisariato.Servicios
+{
+    public class PermisosService
+    {
+        private readonly string connectionString;
+        public PermisosService(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("ConnectionComisariato");
+        }
+        public async Task<bool> TienePermiso(string currentUser, string currentFormName)
+        {
+            int roleId = ObtenerFormIdPorFormNombre(currentUser);
+            int formId = ObtenerRoleIdPorUsuarioNombre(currentFormName);
+
+            if (roleId != -1 && formId != -1)
+            {
+                using var connection = new SqlConnection(connectionString);
+                try
+                {
+                    int TienePermiso = await connection.ExecuteScalarAsync<int>(@"
+                    EXEC ObtenerRecuentoPermisos @RoleId, @FormId
+                ", new
+                    {
+                        RoleId = roleId,
+                        FormId = formId
+                    });
+                    return TienePermiso > 0;
+                }
+                catch (Exception ex)
+                {
+                    // Manejo de errores
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public int ObtenerFormIdPorFormNombre(string formName)
+        {
+            // Implementación para obtener FormId basado en el nombre del formulario
+            return 1; // Valor de ejemplo
+        }
+        public int ObtenerRoleIdPorUsuarioNombre(string username)
+        {
+            // Implementación para obtener RoleId basado en el username
+            return 1; // Valor de ejemplo
+        }
+
+
+
+    }
+
+}
